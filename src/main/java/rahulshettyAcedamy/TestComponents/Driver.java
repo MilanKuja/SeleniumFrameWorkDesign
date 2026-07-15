8887a1756ce445c40979040fe693b4e4a208410a
MilanKuja <ilmilan95@gmail.com>
2026-07-15 12:30:10 +0200

    added Driver class

---
+++ src/main/java/rahulshettyAcedamy/TestComponents/Driver.java
@@ -0,0 +1,56 @@
+package rahulshettyAcedamy.TestComponents;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Driver {
+
+    private static WebDriver driver;
+
+    public static  WebDriver getDriver() {
+        return driver;
+
+    }
+
+    public static WebDriver createDriver() {
+        if (getDriver()==null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("start-maximized");
+            options.addArguments("--disable-notifications");
+//            options.addArguments("--incognito"); --- pokretanje u incognito mode-u
+//            options.addArguments("--headless=new"); --- pokretanje bez header-a
+//            options.addArguments("--disable-extensions"); --- Iskljucuje sve chrome ekstenzije
+//            options.addArguments("--disable-popup-blocking");  --- Iskljucivanje popup-ova
+//            options.setAcceptInsecureCerts(true); --- Koristi se kada se testira development server
+//            options.addArguments("--guest"); --- Pokrece se kao Guest korisnik
+//            options.addArguments("--lang=sr"); --- za testiranje lokalizacija
+
+            Map<String, Object> prefs = new HashMap<>();
+            prefs.put("download.default-directory", System.getProperty("user.dir") + "/downloads");
+            prefs.put("download.prompt_for_download", false);
+            prefs.put("safebrowsing.enabled", true);
+
+
+            options.setExperimentalOption("prefs", prefs);
+//          prefs.put("credentials_enable_service", false); --- gasi servis za upravljanje kredencijalima.
+//          prefs.put("profile.password_manager_enabled", false); --- gasi prikaz Password Manager funkcionalnosti u profilu.
+//          prefs.put("profile.default_content_setting_values.geolocation", 2); -- gasenje lokacija
+//          prefs.put("profile.default_content_setting_values.media_stream_camera", 2); -- gasenje kamere
+//          prefs.put("profile.default_content_setting_values.media_stream_mic", 2); -- gasenje mikrofona
+
+            driver= new ChromeDriver(options);
+        }
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver!=null) {
+            driver.quit();
+            driver = null;
+        }
+    }
+}
